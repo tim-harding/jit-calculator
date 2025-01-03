337dc3accb22042983fac08ea55c5452f0d547f9
Tim Harding <Tim@TimHarding.co>
2025-01-03 14:29:19 -0800

    fix: Box the bench input array
    Addresses a stack overflow on MacOS from too large an array.

--- benches/benches.rs
+++ benches/benches.rs
@@ -6,7 +6,7 @@ const CODE: &str = "+*-/--**++//";
 fn criterion_benchmark(c: &mut Criterion) {
     let p = Program::parse(CODE).unwrap();
     let j = p.jit();
-    let input: [f64; 1_000_000] = std::array::from_fn(|i| i as f64);
+    let input: Box<[f64; 1_000_000]> = Box::new(std::array::from_fn(|i| i as f64));
 
     c.bench_function(format!("interpret {CODE}").as_str(), |b| {
         b.iter(|| {
